64f66a79d818561b865d2779db36b55b48ef7d9a
Kien Vu <vtk5995@gmail.com>
2016-08-15 17:18:58 +0700

    ! fix a bug: deltaTime in android is not accuracy

--- SKTGameSolution/app.cpp
+++ SKTGameSolution/app.cpp
@@ -1,111 +1,107 @@
 #include <time.h>
-#include <string.h>
+#include <chrono>
 
 #include "app.h"
 #include "jni_base.h"
 
-static JavaVM* cached_jvm;
+static JavaVM *cached_jvm;
 
 extern int GameInit();
+
 extern void GameSetScaleFactor(int w, int h);
+
 extern void GameDraw();
+
 extern void GameUpdate(float);
+
 extern void OnKeyEvent(unsigned char, bool);
+
 extern void GameCleanUp();
+
 extern void OnTouchEvent(int, int, int, int);
 
-void appInit()
-{
+void appInit() {
 
     GameInit();
 }
 
-void appResize(int w, int h)
-{
+void appResize(int w, int h) {
     GameSetScaleFactor(w, h);
 }
 
-void appUpdate()
-{
-	static clock_t begin = clock();
-	clock_t end = clock();
+void appUpdate() {
+    auto now = std::chrono::system_clock::now().time_since_epoch();
 
-    GameUpdate( (float) (end - begin)/CLOCKS_PER_SEC);
+    static long long begin = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+    long long end = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+
+    float time = (float) (end - begin) / 1000.0f;
+
+    GameUpdate(time);
     GameDraw();
 
-	begin = end;
+    begin = end;
 }
 
-void appDestroy()
-{
+void appDestroy() {
     GameCleanUp();
 }
 
-void appOnTouch( int type, int x, int y, int id)
-{
+void appOnTouch(int type, int x, int y, int id) {
     LOGI("Touch at [x:%d, y:%d] [id: %d] [type: %d]", x, y, id, type);
-	OnTouchEvent( type, x, y, id);
+    OnTouchEvent(type, x, y, id);
 }
 
-void appOnKeyDown(int keyCode)
-{
+void appOnKeyDown(int keyCode) {
 }
 
-void appOnKeyUp(int keyCode)
-{
+void appOnKeyUp(int keyCode) {
 }
 
-void appPause()
-{
+void appPause() {
     //TODO: pause the game play
 }
 
-void appResume()
-{
+void appResume() {
     //TODO: resume the game play
 }
 
 ///////////////////////////////////////////////////////////////////////
 // TO DO: Clean this up
 ///////////////////////////////////////////////////////////////////////
-jint JNI_OnLoad(JavaVM* vm, void* reserved)
-{
+jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     LOGI("JNI_OnLoad");
 
-	cached_jvm = vm;
-	JNIEnv *env;
+    cached_jvm = vm;
+    JNIEnv *env;
 
-	if (vm->GetEnv((void**) &env, JNI_VERSION_1_4) != JNI_OK)
-	{
-	    LOGE("vm->GetEnv((void**) &env, JNI_VERSION_1_4) FAIL");
-		return -1;
-	}
+    if (vm->GetEnv((void **) &env, JNI_VERSION_1_4) != JNI_OK) {
+        LOGE("vm->GetEnv((void**) &env, JNI_VERSION_1_4) FAIL");
+        return -1;
+    }
 
-	LOGI("return JNI_VERSION_1_4;");
-	return JNI_VERSION_1_4;
+    LOGI("return JNI_VERSION_1_4;");
+    return JNI_VERSION_1_4;
 }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-JNIEnv* get_env()
-{
+JNIEnv *get_env() {
     JNIEnv *env;
 
     int status = cached_jvm->AttachCurrentThread(&env, 0);
-    if (status < 0)
-    {
+    if (status < 0) {
         LOGE("Error attaching thread\n");
         return NULL;
     }
 
-	if (cached_jvm->GetEnv((void **)&env, JNI_VERSION_1_6))
-	{
-		LOGI("JNI version's not supported\n");
-		return NULL;
-	}
+    if (cached_jvm->GetEnv((void **) &env, JNI_VERSION_1_6)) {
+        LOGI("JNI version's not supported\n");
+        return NULL;
+    }
 
-	return env;
+    return env;
 }
 
 
